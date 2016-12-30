94536beace3f77b32df82a54d59d1ed56f73584f
李俊鹏 <jun-peng.li@utsoft.cn>
2016-12-30 10:21:00 +0800

    添加6.0权限处理

--- AdDemo/app/src/main/java/cn/utsoft/commons/sample/AdActivity.java
+++ AdDemo/app/src/main/java/cn/utsoft/commons/sample/AdActivity.java
@@ -1,24 +1,30 @@
 package cn.utsoft.commons.sample;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
 
 import cn.utsoft.commons.ADView.UTAdView;
+import pub.devrel.easypermissions.EasyPermissions;
 
 
-public class AdActivity extends AppCompatActivity {
+public class AdActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
     private UTAdView myView;
     // SD卡中的保存路径
     private String sdpath = Environment.getExternalStorageDirectory() + "/adVideo";
-
+    private static final int REQUEST_CODE_PERMISSION_AD = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
+        showAdView();
     }
 
     private void initView() {
@@ -57,5 +63,31 @@ public class AdActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         myView.cancelCountDownTimer();
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        initView();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        if (requestCode == REQUEST_CODE_PERMISSION_AD) {
+            Toast.makeText(this, "您拒绝了读取sd卡的相关权限!", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    private void showAdView() {
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (!EasyPermissions.hasPermissions(this, perms)) {
+            EasyPermissions.requestPermissions(this, "该应用需要读写sd卡权限", REQUEST_CODE_PERMISSION_AD, perms);
+        }else {
+            initView();
+        }
     }
 }
